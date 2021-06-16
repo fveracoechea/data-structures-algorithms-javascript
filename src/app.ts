420009edb62c951dc1e60f003eccf41b7099acac
Francisco <f.v.borgia@gmail.com>
2021-06-15 20:17:59 -0400

    Added LinkedList data structure

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,36 @@
 import bigOArrays from './BigO/arrays';
+import LinkedList from './classes/LinkedList';
 
+// bigOArrays();
 
-bigOArrays();
+const myLinkedList = new LinkedList(0);
+
+// console.log('----- PUSH -----')
+// console.log(myLinkedList.push(4));
+
+// console.log('----- POP -----');
+// console.log(myLinkedList.pop());
+// console.log(myLinkedList.pop());
+// console.log(myLinkedList);
+
+// console.log('----- UNSHIFT -----');
+// console.log(myLinkedList.push(6).push(9).push(7));
+// console.log(myLinkedList.unshift(4));
+
+// console.log('----- SHIFT -----');
+// console.log(myLinkedList.push(2));
+// console.log(myLinkedList.shift());
+// console.log(myLinkedList);
+
+// console.log('----- GET -----');
+// console.log(myLinkedList.push(1).push(2).push(3));
+// console.log(myLinkedList.get(-2));
+// console.log(myLinkedList.get(12));
+// console.log(myLinkedList.get(2));
+
+console.log('----- SET -----');
+console.log(myLinkedList.push(1).push(2).push(3));
+console.log(myLinkedList.set(-2, 77));
+console.log(myLinkedList.set(12, 77));
+console.log(myLinkedList.set(2, 77));
+console.log(myLinkedList.get(2));
---
+++ src/classes/LinkedList.ts
@@ -0,0 +1,138 @@
+class Node<A> {
+  public value: A;
+  public next: Node<A> | null;
+
+  constructor(value: A) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// * LinkedList are useful when we need to add or remove items from the begening of the list.
+export default class LinkedList<A> {
+
+  public head: Node<A> | null;
+  public tail: Node<A> | null;
+  public length: number;
+
+  constructor(value: A) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+  /** Creates a new node which is added to the end */
+  push(value: A) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  /** Removed the last node from the list and returns it */
+  pop() {
+    if (!this.head) return null;
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next!;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
+  /** Creates a new node which is added to the beginning */
+  unshift(value: A) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  /** Removes the first node from the list and returns it */
+  shift() {
+    if (!this.head) return null;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
+  /** Gets the node that is at a particular index */
+  get(index: number) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp!.next;
+    }
+    return temp;
+  }
+  /** Updates the node that is at a particular index */
+  set(index: number, value: A) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+  /** Creates a new node which is inserted in a specific index */
+  insert(index: number, value: A) {
+    if (index < 0 || index > this.length) return null;
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    const newNode = new Node(value);
+    const temp = this.get(index - 1);
+    newNode.next = temp!.next;
+    temp!.next = newNode;
+    return this;
+  }
+  /** Deletes the node which is at a particular index and returns it */
+  remove(index: number) {
+    if (index < 0 || index > this.length) return null;
+    if (index === 0) return this.shift();
+    if (index === this.length) return this.pop();
+    const before = this.get(index - 1);
+    const temp = before!.next;
+    before!.next = temp!.next;
+    temp!.next = null;
+    this.length--;
+    return temp;
+  }
+  /** Reverses the linked list */
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp!.next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = temp!.next;
+      temp!.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
+}
